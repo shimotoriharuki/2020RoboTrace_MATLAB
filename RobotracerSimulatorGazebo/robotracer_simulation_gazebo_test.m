de54698f4362fe1637de532d54121783533e010c
Haruki <underbird.h@gmail.com>
2020-12-26 16:56:58 +0900

    Successful reset model

--- RobotracerSimulatorGazebo/robotracer_simulation_gazebo_test.m
+++ RobotracerSimulatorGazebo/robotracer_simulation_gazebo_test.m
@@ -10,8 +10,8 @@ cmd_vel_pub = rospublisher('/cmd_vel', 'geometry_msgs/Twist');
 cmd_vel_msg = rosmessage(cmd_vel_pub);
 
 %% メッセージをパプリッシュする
-cmd_vel_msg.Linear.X = 1.;
-cmd_vel_msg.Angular.Z = 1.;
+cmd_vel_msg.Linear.X = -0.5;
+cmd_vel_msg.Angular.Z = 0.5;
 
 send(cmd_vel_pub, cmd_vel_msg);
 
@@ -35,6 +35,11 @@ pose.z = msg.Pose.Pose.Position.Z;
 
 pose
 
+
+%% リスポーン
+testclient = rossvcclient('/gazebo/reset_world');
+testreq = rosmessage(testclient);
+testresp = call(testclient,testreq,'Timeout',3);
 
 %% 停止させる
 cmd_vel_msg.Linear.X = 0.0;
