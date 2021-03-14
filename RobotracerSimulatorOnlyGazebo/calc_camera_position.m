1238893fa9506adcb42218687da54be8ab5c0abb
Haruki <underbird.h@gmail.com>
2021-03-14 14:38:58 +0900

    Gazeboでライントレースできた

--- RobotracerSimulatorOnlyGazebo/calc_camera_position.m
+++ RobotracerSimulatorOnlyGazebo/calc_camera_position.m
@@ -1,5 +1,5 @@
 center_position = [0.15 - 0.08; 0]; % [x; y]
-sensor_num = 7; % 奇数
+sensor_num = 15; % 奇数
 angle = deg2rad(120); % deg
 angle_step = (angle / 2) / ((sensor_num - 1) / 2);
 thetas = 0 : angle_step : angle / 2;
